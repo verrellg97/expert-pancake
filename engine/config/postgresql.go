a6bbbaafae2c903f9e821acde275724379a4c2e7
calvin <calvinkmts@gmail.com>
2022-10-29 16:00:58 -0400

    feat(engine config): helper for postgresql and server config

--- engine/config/postgresql.go
+++ engine/config/postgresql.go
@@ -1,1 +1,21 @@
 package config
+
+type PostgresqlConfig struct {
+	Host     string `mapstructure:"host"`
+	DbName   string `mapstructure:"db_name"`
+	Username string `mapstructure:"username"`
+	Password string `mapstructure:"password"`
+	Ssl      string `mapstructure:"ssl"`
+}
+
+type ConnectionSpec struct {
+	DriverName     string
+	DataSourceName string
+}
+
+// GetConnectionSpec ...
+func (ths PostgresqlConfig) GetConnectionSpec() ConnectionSpec {
+	return ConnectionSpec{
+		DriverName:     "postgres",
+		DataSourceName: "postgres://" + ths.Username + ":" + ths.Password + "@" + ths.Host + "/" + ths.DbName + "?sslmode=" + ths.Ssl}
+}
