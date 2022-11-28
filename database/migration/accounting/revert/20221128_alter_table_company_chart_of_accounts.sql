2a5706b2b749d47c21390cfafc5f3a399284cf2f
Verrell <verrellgunawan97@gmail.com>
2022-11-28 16:55:16 +0700

    feat(database migration accounting) update database migration for accounting service

---
+++ database/migration/accounting/revert/20221128_alter_table_company_chart_of_accounts.sql
@@ -0,0 +1,9 @@
+-- Revert accounting:20221128_alter_table_company_chart_of_accounts from pg
+
+BEGIN;
+
+ALTER TABLE "accounting"."company_chart_of_accounts" ALTER COLUMN is_deleted DROP DEFAULT;
+ALTER TABLE "accounting"."company_chart_of_accounts" ALTER COLUMN is_deleted TYPE INT USING CASE WHEN is_deleted = FALSE THEN 0 ELSE 1 END;
+ALTER TABLE "accounting"."company_chart_of_accounts" ALTER COLUMN is_deleted SET DEFAULT 0;
+
+COMMIT;
