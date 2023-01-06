37771a14845fb96b4d0eae871ceffc6505683598
Verrell <verrellgunawan97@gmail.com>
2023-01-07 05:09:35 +0700

    feat(service business relation) add new endpoint getContactBook

---
+++ service/business-relation/impl/getContactBooks.go
@@ -0,0 +1,76 @@
+package impl
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/calvinkmts/expert-pancake/engine/errors"
+	"github.com/calvinkmts/expert-pancake/engine/httpHandler"
+	"github.com/expert-pancake/service/business-relation/model"
+	"github.com/expert-pancake/service/business-relation/util"
+)
+
+func (a businessRelationService) GetContactBooks(w http.ResponseWriter, r *http.Request) error {
+
+	var req model.GetContactBooksRequest
+	httpHandler.ParseHTTPRequest(r, &req)
+
+	errMapRequest := a.validator.Validate(req)
+	if errMapRequest != nil {
+		return errors.NewClientError().WithDataMap(errMapRequest)
+	}
+
+	result, err := a.dbTrx.GetContactBooks(context.Background(), req.CompanyId)
+	if err != nil {
+		return errors.NewServerError(model.GetContactBooksError, err.Error())
+	}
+
+	var contact_books = make([]model.ContactBook, 0)
+
+	for _, d := range result {
+		resultBranches, err := a.dbTrx.GetContactBookBranches(context.Background(), d.ID)
+		if err != nil {
+			return errors.NewServerError(model.GetContactBooksError, err.Error())
+		}
+		var contact_book = model.ContactBook{
+			ContactBookId:      d.ID,
+			PrimaryCompanyId:   d.PrimaryCompanyID,
+			SecondaryCompanyId: d.SecondaryCompanyID,
+			ContactGroupId:     d.ContactGroupID,
+			Name:               d.Name,
+			Email:              d.Email,
+			Phone:              d.Phone,
+			Mobile:             d.Mobile,
+			Web:                d.Web,
+			AdditionalInfo: model.ContactBookAdditionaInfo{
+				Nickname: d.Nickname,
+				Tag:      d.Tag,
+				Note:     d.Note,
+			},
+			MailingAddress: model.ContactBookAddress{
+				Province:    d.MailingProvince,
+				Regency:     d.MailingRegency,
+				District:    d.MailingDistrict,
+				PostalCode:  d.MailingPostalCode,
+				FullAddress: d.MailingFullAddress,
+			},
+			ShippingAddress: model.ContactBookAddress{
+				Province:    d.ShippingProvince,
+				Regency:     d.ShippingRegency,
+				District:    d.ShippingDistrict,
+				PostalCode:  d.ShippingPostalCode,
+				FullAddress: d.ShippingFullAddress,
+			},
+			IsAllBranches: d.IsAllBranches,
+			Branches:      util.ContactBookBranchDbToApi(resultBranches),
+			IsCustomer:    d.IsCustomer,
+			IsSupplier:    d.IsSupplier,
+		}
+		contact_books = append(contact_books, contact_book)
+	}
+
+	res := contact_books
+	httpHandler.WriteResponse(w, res)
+
+	return nil
+}
