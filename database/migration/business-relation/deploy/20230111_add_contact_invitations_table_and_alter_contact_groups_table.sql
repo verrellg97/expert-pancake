7751ff7513b001110647a6ed28af60270a88ecff
Verrell <verrellgunawan97@gmail.com>
2023-01-11 02:26:28 +0700

    feat(database migration) add table contact_invitations and alter table contact_groups

---
+++ database/migration/business-relation/deploy/20230111_add_contact_invitations_table_and_alter_contact_groups_table.sql
@@ -0,0 +1,27 @@
+-- Deploy business-relation:20230111_add_contact_invitations_table_and_alter_contact_groups_table to pg
+
+BEGIN;
+
+ALTER TABLE "business_relation"."contact_groups" ADD COLUMN image_url TEXT NOT NULL DEFAULT '';
+ALTER TABLE "business_relation"."contact_groups" ADD COLUMN description TEXT NOT NULL DEFAULT '';
+
+CREATE TYPE "business_relation"."contact_invitation_status" AS ENUM (
+  'waiting',
+  'cancelled',
+  'accepted',
+  'rejected'
+);
+
+CREATE TABLE "business_relation"."contact_invitations" (
+  "id" text NOT NULL,
+  "primary_contact_book_id" text NOT NULL DEFAULT '',
+  "secondary_contact_book_id" text NOT NULL DEFAULT '',
+  "primary_company_id" text NOT NULL,
+  "secondary_company_id" text NOT NULL,
+  "status" business_relation.contact_invitation_status NOT NULL DEFAULT 'waiting',
+  "created_at" timestamptz DEFAULT (now()),
+  "updated_at" timestamptz DEFAULT (now()),
+  PRIMARY KEY ("id")
+);
+
+COMMIT;
