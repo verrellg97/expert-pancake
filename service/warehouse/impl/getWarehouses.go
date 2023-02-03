094e82f39371aef2620a14b49260880460d6d850
Verrell <m26415079@john.petra.ac.id>
2023-02-03 18:04:52 +0700

    fix(service warehouse) update decision for id request

--- service/warehouse/impl/getWarehouses.go
+++ service/warehouse/impl/getWarehouses.go
@@ -23,14 +23,9 @@ func (a warehouseService) GetWarehouses(w http.ResponseWriter, r *http.Request) 
 
 	var isFilterId = false
 	var id = ""
-	if req.Id != nil {
-		switch *req.Id {
-		case "":
-			isFilterId = false
-		default:
-			id = *req.Id
-			isFilterId = true
-		}
+	if req.Id != nil && *req.Id != "" {
+		id = *req.Id
+		isFilterId = true
 	}
 
 	result, err := a.dbTrx.GetWarehouses(context.Background(), db.GetWarehousesParams{
