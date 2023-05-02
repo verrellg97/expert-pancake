492239010006f6085b4d8087c00688e55bedc611
Verrell <m26415079@john.petra.ac.id>
2023-05-02 13:22:33 +0700

    feat(purchasing service) update endpoint get purchase order

--- service/purchasing/impl/getPurchaseOrders.go
+++ service/purchasing/impl/getPurchaseOrders.go
@@ -23,6 +23,15 @@ func (a purchasingService) GetPurchaseOrders(w http.ResponseWriter, r *http.Requ
 		return errors.NewClientError().WithDataMap(errMapRequest)
 	}
 
+	argWarehouseIds := client.GetWarehousesRequest{
+		BranchId: req.BranchId,
+	}
+	warehouseIds, err := client.GetWarehouses(argWarehouseIds)
+	if err != nil {
+		return errors.NewServerError(model.GetPurchaseOrdersError, err.Error())
+	}
+	warehouseMap := util.WarehouseApiToMap(warehouseIds.Result.Warehouses)
+
 	result, err := a.dbTrx.GetPurchaseOrders(context.Background(), db.GetPurchaseOrdersParams{
 		CompanyID: req.CompanyId,
 		BranchID:  req.BranchId,
@@ -46,19 +55,23 @@ func (a purchasingService) GetPurchaseOrders(w http.ResponseWriter, r *http.Requ
 		}
 
 		var purchaseOrder = model.PurchaseOrder{
-			TransactionId:      d.ID,
-			CompanyId:          d.CompanyID,
-			BranchId:           d.BranchID,
-			FormNumber:         d.FormNumber,
-			TransactionDate:    d.TransactionDate.Format(util.DateLayoutYMD),
-			ContactBookId:      d.ContactBookID,
-			SecondaryCompanyId: d.SecondaryCompanyID,
-			SupplierName:       contactBook.Result[0].Name,
-			KonekinId:          d.KonekinID,
-			CurrencyCode:       d.CurrencyCode,
-			TotalItems:         strconv.FormatInt(d.TotalItems, 10),
-			Total:              strconv.FormatInt(d.Total, 10),
-			Status:             d.Status,
+			TransactionId:             d.ID,
+			CompanyId:                 d.CompanyID,
+			BranchId:                  d.BranchID,
+			FormNumber:                d.FormNumber,
+			TransactionDate:           d.TransactionDate.Format(util.DateLayoutYMD),
+			ContactBookId:             d.ContactBookID,
+			SecondaryCompanyId:        d.SecondaryCompanyID,
+			SupplierName:              contactBook.Result[0].Name,
+			KonekinId:                 d.KonekinID,
+			CurrencyCode:              d.CurrencyCode,
+			ShippingDate:              d.ShippingDate.Format(util.DateLayoutYMD),
+			ReceivingWarehouseId:      d.ReceivingWarehouseID,
+			ReceivingWarehouseName:    warehouseMap[d.ReceivingWarehouseID].Name,
+			ReceivingWarehouseAddress: warehouseMap[d.ReceivingWarehouseID].Address,
+			TotalItems:                strconv.FormatInt(d.TotalItems, 10),
+			Total:                     strconv.FormatInt(d.Total, 10),
+			Status:                    d.Status,
 		}
 		purchaseOrders = append(purchaseOrders, purchaseOrder)
 	}
