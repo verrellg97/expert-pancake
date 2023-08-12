d862cc04b740cfdfabe42b9b33df291d1d047b30
Verrell <m26415079@john.petra.ac.id>
2023-08-13 03:11:25 +0700

    chore(sales service) update endpoint upsertPOS

--- service/sales/impl/client/inventory_request.go
+++ service/sales/impl/client/inventory_request.go
@@ -11,3 +11,23 @@ type GetItemUnitsRequest struct {
 	ItemId  string `json:"item_id" validate:"required"`
 	Keyword string `json:"keyword"`
 }
+
+type InsertStockMovementRequest struct {
+	TransactionId        string `json:"transaction_id" validate:"required"`
+	CompanyId            string `json:"company_id" validate:"required"`
+	BranchId             string `json:"branch_id" validate:"required"`
+	TransactionCode      string `json:"transaction_code" validate:"required"`
+	TransactionDate      string `json:"transaction_date" validate:"required"`
+	TransactionReference string `json:"transaction_reference" validate:"required"`
+	DetailTransactionId  string `json:"detail_transaction_id" validate:"required"`
+	WarehouseId          string `json:"warehouse_id" validate:"required"`
+	WarehouseRackId      string `json:"warehouse_rack_id" validate:"required"`
+	VariantId            string `json:"variant_id" validate:"required"`
+	ItemBarcodeId        string `json:"item_barcode_id" validate:"required"`
+	Amount               string `json:"amount" validate:"required"`
+}
+
+type DeleteStockMovementRequest struct {
+	TransactionId        string `json:"transaction_id" validate:"required"`
+	TransactionReference string `json:"transaction_reference" validate:"required"`
+}
